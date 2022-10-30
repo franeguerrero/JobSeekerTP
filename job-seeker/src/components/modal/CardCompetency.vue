adb9230709db42fcbdc004dc26e1730fd1689ca8
Francisco Guerrero <franeguerrero@gmail.com>
2022-10-30 16:20:22 -0300

    Added modal to home

---
+++ job-seeker/src/components/modal/CardCompetency.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <div class="competency">
+        <span><p> Edad: {{age}}</p></span>
+        <span><p> Jornada: {{fulltime ? "Full-Time":"Part-Time"}}  </p></span>
+        <span><p> Movilidad: {{movilidad ? "Si":"No"}}   </p></span>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'CardCompetencty',
+        props: {
+            age: Number,
+            fulltime: Boolean,
+            movility: Boolean,
+        },
+    }
+</script>
+
+<style scoped>
+
+    .competency{  
+        display:flex;
+        flex-direction:column;
+        align-items:baseline;
+        justify-content: center;
+    }
+    .competency span {
+        margin: 5px;
+    }
+
+
+</style>
---
+++ job-seeker/src/components/modal/CardSkills.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="skills">
+        <h3 class="information-title"> Habilidades </h3>
+        <div class="skill">
+            <p v-for="skill in skills" :key="skill" > {{skill}} </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CardSkills',
+        props:{
+            skills: Array,
+        }
+    }
+</script>
+
+<style  scoped>
+
+    .skills{
+        display:flex;
+        flex-direction:column;
+        margin-top: 20px;
+    }
+    .information-title{
+        font-size: 1.4em;
+        margin-bottom: 10px;
+    }
+    .skill{  
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+    }
+    .skill p{
+        margin: 5px;
+
+    }
+</style>
